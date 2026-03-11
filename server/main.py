d45e3c37c1c6942d710763f33fd0d1c45b90b9d4
Karan Sharma <ks0764511@gmail.com>
2026-03-11 22:41:08 +0530

    CROS FIXED

--- server/main.py
+++ server/main.py
@@ -11,8 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://autisum-prediction-app.vercel.app"
-]
+    "https://autisum-prediction-app.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
