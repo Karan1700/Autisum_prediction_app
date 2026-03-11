f87d9c353b3ffa7ce154f4da559c45fbd4cc1850
Karan Sharma <ks0764511@gmail.com>
2026-03-11 22:35:12 +0530

    Fixed CORS for Vercel frontend

--- server/main.py
+++ server/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # Allow React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://autisum-prediction-app.vercel.app"
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
